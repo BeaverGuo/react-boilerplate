0a5eefb0baa9f3e72306ae12f60e2359883cfe80
lalabeaver21 <lalabeaver21@gmail.com>
2016-10-31 00:31:24 +0800

    add git config core.autocrlf false

--- other_notes/every_day_log.js
+++ other_notes/every_day_log.js
@@ -433,3 +433,31 @@ gulp.task('clean', () => del([
 ]));
 //将 /dist/client-bundle.js* 添加到 .gitignore 文件。
 
+/*git error
+In Unix systems the end of a line is represented with a line feed (LF). In windows a line is represented with a carriage return (CR) and a line feed (LF) thus (CRLF). when you get code from git that was uploaded from a unix system they will only have a LF. It's nothing to worry about.
+
+And if you want to turn this warning off, type this in the git command line
+
+git config core.autocrlf true
+If you want to make an intelligent decision how git should handle this, read the documentation
+
+Here is a snippet
+
+Formatting and Whitespace
+
+Formatting and whitespace issues are some of the more frustrating and subtle problems that many developers encounter when collaborating, especially cross-platform. It’s very easy for patches or other collaborated work to introduce subtle whitespace changes because editors silently introduce them, and if your files ever touch a Windows system, their line endings might be replaced. Git has a few configuration options to help with these issues.
+
+core.autocrlf
+If you’re programming on Windows and working with people who are not (or vice-versa), you’ll probably run into line-ending issues at some point. This is because Windows uses both a carriage-return character and a linefeed character for newlines in its files, whereas Mac and Linux systems use only the linefeed character. This is a subtle but incredibly annoying fact of cross-platform work; many editors on Windows silently replace existing LF-style line endings with CRLF, or insert both line-ending characters when the user hits the enter key.
+
+Git can handle this by auto-converting CRLF line endings into LF when you add a file to the index, and vice versa when it checks out code onto your filesystem. You can turn on this functionality with the core.autocrlf setting. If you’re on a Windows machine, set it to true – this converts LF endings into CRLF when you check out code:
+
+$ git config --global core.autocrlf true
+If you’re on a Linux or Mac system that uses LF line endings, then you don’t want Git to automatically convert them when you check out files; however, if a file with CRLF endings accidentally gets introduced, then you may want Git to fix it. You can tell Git to convert CRLF to LF on commit but not the other way around by setting core.autocrlf to input:
+
+$ git config --global core.autocrlf input
+This setup should leave you with CRLF endings in Windows checkouts, but LF endings on Mac and Linux systems and in the repository.
+
+If you’re a Windows programmer doing a Windows-only project, then you can turn off this functionality, recording the carriage returns in the repository by setting the config value to false:
+
+$ git config --global core.autocrlf false

c39df01a9842086823124f760ce5d22396e806ac
xianguo <xianguo@Ctrip.com>
2017-08-22 10:12:42 +0800

    add

---
+++ other_notes/30-challenge/data_type.py
@@ -0,0 +1,25 @@
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# i_from_user_input = 12
+# d_from_user_input = 4.0
+# s_from_user_input = 'is the best place to learn and practice coding!'
+# Read and save an integer, double, and String to your variables.
+
+# get user input from console
+i_from_user_input = input('')
+d_from_user_input = input('')
+s_from_user_input = input('')
+
+# Print the sum of both integer variables on a new line.
+sum_of_i = i + int(i_from_user_input)
+sum_of_d = d + float(d_from_user_input)
+concat_s = s + s_from_user_input
+# Print the sum of the double variables on a new line.
+print(sum_of_i)
+print(sum_of_d)
+print(s + s_from_user_input)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
---
+++ other_notes/30-challenge/hello_word.py
@@ -0,0 +1,1 @@
+#In Object-Oriented programming, a method is a type of function that operates on the fields of a class.
